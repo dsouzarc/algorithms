05f46b47136635f78e9ef66781eecb05df407ddc
Ryan D'souza <rdsouza@hs-242-imac-21.internal>
2014-11-20 14:03:37 -0500

    First classes for trie

---
+++ Trie.java
@@ -0,0 +1,7 @@
+/** 
+ * Written by Ryan D'souza
+ * Represents a Trie, a data structure composed of
+ * TrieNodes. Will be used for storing words */
+
+public class Trie { 
+}
---
+++ TrieNode.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+/** 
+ * Written by Ryan D'souza
+ * Represents a TrieNode, the element
+ * that composes a Trie list for storing
+ * words from a dictionary */
+
+public class TrieNode { 
+    private TrieNode children;
+    private TrieNode parent;
+
+}
