4b62d2c0529b062057c1c2bc562d38beb505281e
Ryan D'souza <dsouzarc@gmail.com>
2014-11-21 17:13:45 -0500

    Workinggit add --all

--- Trie.java
+++ Trie.java
@@ -1,7 +1,43 @@
+import java.util.LinkedList;
 /** 
  * Written by Ryan D'souza
  * Represents a Trie, a data structure composed of
  * TrieNodes. Will be used for storing words */
 
 public class Trie { 
+    private final TrieNode root;
+
+    public Trie() { 
+        this.root = new TrieNode();
+    }
+
+    public void addWord(final String word) { 
+        this.root.addWord(word.toLowerCase());
+    }
+
+    public LinkedList getWordsWithPrefix(final String prefix) { 
+        TrieNode valNodes = root;
+
+        for(Character c : prefix.toCharArray()) { 
+            valNodes = valNodes.getNode(c);
+
+            if(valNodes == null) {
+                return new LinkedList();
+            }
+        }
+        return valNodes.getWords();
+    }
+
+    public static void main(String[] ryan) { 
+        Trie me = new Trie();
+        me.addWord("ryan");
+        me.addWord("ryannayr");
+
+        final LinkedList<String> r = me.getWordsWithPrefix("rya");
+        System.out.println(r.size());
+
+        for(String s : r) { 
+            System.out.println(s);
+        }
+    }
 }
--- TrieNode.java
+++ TrieNode.java
@@ -7,7 +7,98 @@ import java.util.LinkedList;
  * words from a dictionary */
 
 public class TrieNode { 
-    private TrieNode children;
+    private TrieNode[] children;
     private TrieNode parent;
 
+    //Last character of word
+    private boolean isWord;
+
+    //Other children
+    private boolean isLeaf;
+
+    private char value;
+
+    /** Top level Node constructor */
+    public TrieNode() { 
+        this.children = new TrieNode[26];
+        this.isLeaf = true;
+        this.isWord  = false;
+    }
+
+    /** Children constructor */
+    public TrieNode(final char value) { 
+        this();
+        this.value = value;
+    }
+
+    /** Recursively adds characters as child nodes */
+    public void addWord(String word) { 
+        word = word.toLowerCase();
+
+        //Has children
+        isLeaf = false;
+
+        //ASCII Value
+        int charPosition = word.charAt(0) - 'a';
+
+        //If no children with that constructor exists
+        if(children[charPosition] == null) { 
+            //Make a new Node
+            children[charPosition] = new TrieNode(word.charAt(0));
+            
+            //That node's parent is this
+            children[charPosition].parent = this;
+        }
+        
+        //Recursive part to add word
+        if(word.length() > 1) { 
+            children[charPosition].addWord(word.substring(1));
+        }
+        //If no more characters left, is end
+        else { 
+            children[charPosition].isWord = true;
+        }
+        System.out.println(java.util.Arrays.toString(children) + "\t" + value);
+    }
+
+    /** Return nodes associated with character */
+    public TrieNode getNode(final char toGet) { 
+        final int loc = (int)toGet >= 65 && (int)toGet < 97 ? (int)toGet - 65: (int)toGet - 97;
+        return children[toGet - 'a'];
+    }
+
+    /** Return list of words beneath */
+    public LinkedList getWords() { 
+        final LinkedList words = new LinkedList();
+
+        //If last char of word, add ourself
+        if(isWord) { 
+            System.out.println(toString());
+            words.add(toString());
+        }
+
+        //Get all children
+        if(!isLeaf) { 
+            for(int i = 0; i < this.children.length; i++) { 
+                if(this.children[i] != null) { 
+                    //Add the child
+                    words.addAll(this.children[i].getWords());
+                }
+            }
+        }
+
+        return words;
+    }
+
+    /** Return toString representation (goes to parent
+     * and adds child nodes) */
+    public String toString() { 
+        if(parent == null) { 
+            return "";
+        }
+
+        else { 
+            return parent.toString() + new String(new char[]{value});
+        }
+    }
 }
