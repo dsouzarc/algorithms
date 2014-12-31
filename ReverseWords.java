6a7c7f3d23688827a506e889aad8b68bdd51c419
Ryan D'souza <dsouzarc@gmail.com>
2014-12-31 16:42:51 -0500

    Finished using stack

--- ReverseWords.java
+++ ReverseWords.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /** 
  * Written by Ryan D'souza
  * Reverses the words in a String */
@@ -5,14 +7,39 @@
 public class ReverseWords { 
 
     public static String reverseWords(final String original) { 
+
+        //To hold the result
         final StringBuilder result = new StringBuilder("");
 
-        //Attempt to do this without auxillary arrays/data structures
-        //Step 1: Get number of words
-        final int numWords = original.length() - original.replace(" ", "").length();
+        //Letters for reversing
+        final Stack<Character> letters = new Stack<Character>();
 
+        //Iterate through every letter
+        for(Character c : original.toCharArray()) { 
+
+            letters.push(c);
+
+            //If it is a space, the word has ended
+            if(c == ' ') { 
+
+                //So add everything from the stack back
+                while(!letters.isEmpty()) { 
+                    result.append(letters.pop());
+                }
+            }
+
+        }
         return result.toString();
     }
 
+    public static void main(String[] ryan) { 
 
+        final StringBuilder sentence = new StringBuilder("");
+
+        for(String r : ryan) { 
+            sentence.append(r + " ");
+        }
+
+        System.out.println(reverseWords(sentence.toString()));
+    }
 }
