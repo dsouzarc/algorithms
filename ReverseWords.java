f967198d975153921c03d8f870a73859974c68a5
Ryan D'souza <dsouzarc@gmail.com>
2014-12-31 16:32:38 -0500

    Beginning

--- ReverseWords.java
+++ ReverseWords.java
@@ -4,4 +4,15 @@
 
 public class ReverseWords { 
 
+    public static String reverseWords(final String original) { 
+        final StringBuilder result = new StringBuilder("");
+
+        //Attempt to do this without auxillary arrays/data structures
+        //Step 1: Get number of words
+        final int numWords = original.length() - original.replace(" ", "").length();
+
+        return result.toString();
+    }
+
+
 }
